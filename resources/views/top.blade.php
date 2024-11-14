8a9a5b94afa911d009a431989b810c0ffe3ead05
ashintai <shintai.akira@gmail.com>
2024-11-14 16:42:11 +0900

    add fileread

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-    こんばんわ
+<input type="file" id="fileInput">
+    <pre id="fileContent"></pre>
+
+    <script>
+        document.getElementById('fileInput').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    document.getElementById('fileContent').textContent = e.target.result;
+                };
+                reader.readAsText(file);
+            }
+        });
+    </script>
 </body>
 </html>
